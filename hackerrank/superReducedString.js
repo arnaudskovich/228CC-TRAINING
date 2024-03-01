56b606f91e388336d888b764294ef837c8d47b3c
arnaudskovich <arnaudskovich2@gmail.com>
2024-03-01 21:05:49 +0000

    updated superReducedString.js

--- hackerrank/superReducedString.js
+++ hackerrank/superReducedString.js
@@ -3,8 +3,9 @@
  * @param {string} s
  */
 function resizeMax(s) {
-	const r = /([a-z])\1/g;
-	while (r.test(s)) {
+	const r = /([a-z])\1/g,
+		r_ = /([a-z])\1/;
+	while (r_.test(s)) {
 		s = s.replace(r, "");
 	}
 	return s.length > 0 ? s : "Empty String";
